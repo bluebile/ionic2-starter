4067d3a6571229a4e85919fd37f5a893113f9377
ramonornela <ramon.ornela@gmail.com>
2017-01-05 23:54:19 -0200

    feat(authentication): implementacao result isValid()

--- src/modules/authentication/providers/result.ts
+++ src/modules/authentication/providers/result.ts
@@ -3,8 +3,12 @@ export enum ResultCode {
   SUCCESS
 };
 
-export class Result { 
+export class Result {
   constructor(private code: number, private identity?: any) {}
+
+  isValid() {
+    return this.code > 0;
+  }
 
   getCode(): number {
     return this.code;
