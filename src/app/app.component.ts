5b992fa92fcda2bc8d20aeba1f100ade3cf6da07
ramonornela <ramon.ornela@gmail.com>
2017-01-07 09:07:01 -0200

    chore(autentication): eliminando tests authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 import { HomePage } from '../pages/home/home';
-import { Adapter, Authentication } from '../modules/authentication';
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
@@ -11,13 +10,7 @@ export class MyApp {
 
   rootPage: any;
 
-  constructor(public platform: Platform, adapter: Adapter, auth: Authentication) {
-    adapter.setIdentity('12345678')
-      .setCredential('123456');
-
-    auth.authenticate().catch(() => {
-      console.log('error');
-    });
+  constructor(public platform: Platform) {
     this.platform.ready().then(() => {
       StatusBar.styleDefault();
 
