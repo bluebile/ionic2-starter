c2bd49b8b5a4ef96b46ba1aadefd8f0aff6de763
ramonornela <ramon.ornela@gmail.com>
2017-01-09 20:41:37 -0200

    feat(login): adicao de validators cpf utilizado no BRB implementada por @michael-xd

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,6 @@
-import { Masks } from '../../util/format';
+import { Masks, Validators as ValidatorsInternal } from '../../util';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AlertController, MenuController, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-login',
@@ -9,51 +8,48 @@ import { AlertController, MenuController, NavController } from 'ionic-angular';
 })
 export class LoginPage {
 
-  authForm: FormGroup;
+  form: FormGroup;
   submitAttempted: boolean = false;
 
   mask = Masks.cpf;
 
   constructor(
-    public navController: NavController,
-    formBuilder: FormBuilder,
-    private menuCtrl: MenuController,
-    public alertCtrl: AlertController
+    formBuilder: FormBuilder
   ) {
-    this.authForm = formBuilder.group({
-      cpf: ['', Validators.compose([Validators.required, Validators.maxLength(11), Validators.minLength(11), Validators.pattern('[0-9]*')])],
+    this.form = formBuilder.group({
+      cpf: ['', Validators.compose([Validators.required, ValidatorsInternal.cpf ])],
       password: ['', Validators.compose([Validators.required])],
     });
-
-    /**
-     * Desabilitar menu na tela login
-     * */
-    this.menuCtrl.enable(false);
   }
 
-  validateForm(): boolean {
-    if (this.authForm.valid) {
+  validate(): boolean {
+    if (this.form.valid) {
       return true;
     }
 
     let msg = '';
 
-    if (this.authForm.controls['cpf'].hasError('required')) {
+    if (this.form.controls['cpf'].hasError('required')) {
       msg = 'O CPF é obrigatório';
-    } else if (this.authForm.controls['cpf'].hasError('minLength')
-      || this.authForm.controls['cpf'].hasError('maxLength')
-      || this.authForm.controls['cpf'].hasError('pattern')) {
+    } else if (this.form.controls['cpf'].hasError('minLength')
+      || this.form.controls['cpf'].hasError('maxLength')
+      || this.form.controls['cpf'].hasError('pattern')) {
       msg = 'A CPF é composto de 11 dígitos';
-    } else if (this.authForm.controls['password'].hasError('required')) {
+    } else if (this.form.controls['password'].hasError('required')) {
       msg = 'A senha é obrigatória';
     }
+
+    console.log(msg);
 
     return false;
   }
 
-  login(): void {
+  login(formData: any): void {
     this.submitAttempted = true;
 
-    if (this.validateForm()) {}
+    console.log(formData);
+    if (this.validate()) {
+      console.log(formData);
+    }
   }
 }
