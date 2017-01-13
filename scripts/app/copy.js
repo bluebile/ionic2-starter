c4549cd787f6e4e424b3f35a0f82fbde54e82d0d
ramonornela <ramon.ornela@gmail.com>
2017-01-13 11:44:38 -0200

    chore(devOps): forçando copy para criacao de projeto

--- scripts/app/copy.js
+++ scripts/app/copy.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
 var shelljs       = require('shelljs');
+var path = require('path');
 var npmConfigJson = JSON.parse(process.env.npm_config_argv).original;
 
-shelljs.rm('-rf', npmConfigJson[2]);
-shelljs.cp('-R', './', npmConfigJson[2]);
-shelljs.rm('-rf', npmConfigJson[2] + '/.git');
+var folderTmp = path.join(npmConfigJson[2], '.tmp');
+
+shelljs.rm('-rf', path.join(npmConfigJson[2], '*'));
+shelljs.cp('-R', './', folderTmp);
+shelljs.rm('-rf', path.join(folderTmp, '.git'));
+shelljs.cp('-R', folderTmp + '/.', npmConfigJson[2]);
+shelljs.rm('-rf', folderTmp);
