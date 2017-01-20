b9bf46b8507709316a2705109e689f89b17f4291
fsdrumond <fsdrumond@gmail.com>
2017-01-20 11:09:22 -0200

    feat(logout): adicionada a funcao de logout na tela de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,19 +4,19 @@ import { Component } from '@angular/core';
 import { App } from 'ionic-angular';
 
 @Component({
-	selector: 'page-profile',
-	templateUrl: 'profile.html'
+    selector: 'page-profile',
+    templateUrl: 'profile.html'
 })
 export class ProfilePage {
 
-	constructor(private app: App, private user: User) {
-	}
+    constructor(private app: App, private user: User) {
+    }
 
-	logout() {
-		this.user.confirmLogout().then(() => {
-			this.app.getActiveNav().setRoot(Login);
-		}).catch(() => {
-			// clicado no botao nao
-		});
-	}
+    logout() {
+        this.user.confirmLogout().then(() => {
+            this.app.getActiveNav().setRoot(Login);
+        }).catch(() => {
+            // clicado no botao nao
+        });
+    }
 }
