8e0d97dc1a7251e883bd3378b6169822e37b8185
fsulczinski <fsulczinski@gmail.com>
2017-01-20 11:43:01 -0200

    style(login): formatação do layout básico da tela de login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,94 +1,89 @@
-import { User } from '../../providers/providers';
-import { Masks, Validators as ValidatorsInternal } from '../../util';
-import { Home, Termo } from '../pages';
-import { RecoveryPasswordPage } from '../recoveryPassword/recoveryPassword';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Storage } from '@ionic/storage';
-import { App, LoadingController, ToastController } from 'ionic-angular';
+import {User} from '../../providers/providers';
+import {Masks, Validators as ValidatorsInternal} from '../../util';
+import {Home, Termo} from '../pages';
+import {RecoveryPasswordPage} from '../recoveryPassword/recoveryPassword';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Storage} from '@ionic/storage';
+import {App, LoadingController, ToastController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+    selector: 'page-login',
+    templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  form: FormGroup;
-  submitAttempted: boolean = false;
+    form: FormGroup;
+    submitAttempted: boolean = false;
 
-  mask = Masks.cpf;
+    mask = Masks.cpf;
+    esqueciSenha: any = RecoveryPasswordPage;
 
-  constructor(
-    formBuilder: FormBuilder,
-    private app: App,
-    private loadingCtrl: LoadingController,
-    private storage: Storage,
-    private toastCtrl: ToastController,
-    private user: User
-  ) {
-    this.form = formBuilder.group({
-      cpf: ['', Validators.compose([Validators.required, ValidatorsInternal.cpf ])],
-      password: ['', Validators.compose([Validators.required])],
-    });
-  }
-
-  validate(): boolean {
-    if (this.form.valid) {
-      return true;
+    constructor(formBuilder: FormBuilder,
+                private app: App,
+                private loadingCtrl: LoadingController,
+                private storage: Storage,
+                private toastCtrl: ToastController,
+                private user: User) {
+        this.form = formBuilder.group({
+            cpf: ['', Validators.compose([Validators.required, ValidatorsInternal.cpf])],
+            password: ['', Validators.compose([Validators.required])],
+        });
     }
 
-    let msg = '';
-
-    if (this.form.controls['cpf'].hasError('required')) {
-      msg = 'O CPF é obrigatório';
-    } else if (this.form.controls['cpf'].hasError('invalid')) {
-      msg = 'CPF inválido.';
-    } else if (this.form.controls['password'].hasError('required')) {
-      msg = 'A senha é obrigatória';
-    }
-
-    let toast = this.toastCtrl.create({
-      message: msg,
-      duration: 3000,
-      position: 'top'
-    });
-
-    toast.present();
-
-    return false;
-  }
-
-  login(formData: any): void {
-    this.submitAttempted = true;
-    if (this.validate()) {
-      let loading = this.loadingCtrl.create();
-      loading.present();
-
-      this.user.login(formData.cpf.replace(/\D/g, ''), formData.password).then(() => {
-        loading.dismiss();
-        if (Termo) {
-          this.showTermo();
-          return;
+    validate(): boolean {
+        if (this.form.valid) {
+            return true;
         }
-        this.app.getActiveNav().setRoot(Home);
-      }).catch(() => {
-        loading.dismiss();
-      });
+
+        let msg = '';
+
+        if (this.form.controls['cpf'].hasError('required')) {
+            msg = 'O CPF é obrigatório';
+        } else if (this.form.controls['cpf'].hasError('invalid')) {
+            msg = 'CPF inválido.';
+        } else if (this.form.controls['password'].hasError('required')) {
+            msg = 'A senha é obrigatória';
+        }
+
+        let toast = this.toastCtrl.create({
+            message: msg,
+            duration: 3000,
+            position: 'top'
+        });
+
+        toast.present();
+
+        return false;
     }
-  }
 
-  showTermo() {
-    this.storage.get('_termo').then((data) => {
-      if (data === true) {
-        this.app.getActiveNav().setRoot(Home);
-        return;
-      }
+    login(formData: any): void {
+        this.submitAttempted = true;
+        if (this.validate()) {
+            let loading = this.loadingCtrl.create();
+            loading.present();
 
-      this.app.getActiveNav().setRoot(Termo);
-    });
-  }
+            this.user.login(formData.cpf.replace(/\D/g, ''), formData.password).then(() => {
+                loading.dismiss();
+                if (Termo) {
+                    this.showTermo();
+                    return;
+                }
+                this.app.getActiveNav().setRoot(Home);
+            }).catch(() => {
+                loading.dismiss();
+            });
+        }
+    }
 
-  recoveyPassword() {
-    this.app.getActiveNav().setRoot(RecoveryPasswordPage);
-  }
+    showTermo() {
+        this.storage.get('_termo').then((data) => {
+            if (data === true) {
+                this.app.getActiveNav().setRoot(Home);
+                return;
+            }
+
+            this.app.getActiveNav().setRoot(Termo);
+        });
+    }
 }
