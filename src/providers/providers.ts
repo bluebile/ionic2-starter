4ddb763cf3d5c57ed53cc55b3c50b20f93126cde
fsdrumond <fsdrumond@gmail.com>
2017-01-23 14:52:20 -0200

    refactor(listservice): remocao da antiga provider de acesso a dados da lista

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -1,12 +1,10 @@
 import { ERROR_HANDLER } from './error.handler';
 import { User } from './user';
-import { ListService } from './list.service';
 import { CardService } from './card.service';
 
 export const Providers: any[] = [
     ERROR_HANDLER,
     User,
-    ListService,
     CardService
 ];
 
