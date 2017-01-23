b5128660468f2d2e29a65c61fb7f11d352380e5b
fsulczinski <fsulczinski@gmail.com>
2017-01-23 14:35:55 -0200

    feat(lista): iteração da lista e serviço com mockup

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -1,11 +1,13 @@
 import { ERROR_HANDLER } from './error.handler';
 import { User } from './user';
 import { ListService } from './list.service';
+import { CardService } from './card.service';
 
 export const Providers: any[] = [
     ERROR_HANDLER,
     User,
-    ListService
+    ListService,
+    CardService
 ];
 
 export { User };
