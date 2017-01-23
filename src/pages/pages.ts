c7a4078e8f509eed2c9ec0906327c41f1c5cd42a
fsulczinski <fsulczinski@gmail.com>
2017-01-23 17:06:08 -0200

    feat(user): criacao da pagina de detalhe de usuario

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -5,6 +5,8 @@ import { OnboardPage } from './onboard/onboard';
 import { ProfilePage } from './profile/profile';
 import { RecoveryPasswordPage } from './recoveryPassword/recoveryPassword';
 import { TermoPage } from './termo/termo';
+import { UserDetailPage } from '../pages/userdetail/userDetail';
+
 
 /**
  * The Pages array lists all of the pages we want to use in our app.
@@ -12,15 +14,17 @@ import { TermoPage } from './termo/termo';
  * can find them. As you add and remove pages, make sure to keep this list up to date.
  */
 export const Pages: any[] = [
-  HomePage,
-  LoginPage,
-  TermoPage,
-  ErroConectividadePage,
-  OnboardPage,
-  UserListPage,
-  CardPage,
-  ProfilePage,
-  RecoveryPasswordPage
+    HomePage,
+    LoginPage,
+    TermoPage,
+    ErroConectividadePage,
+    OnboardPage,
+    UserListPage,
+    CardPage,
+    ProfilePage,
+    RecoveryPasswordPage,
+    UserDetailPage
+
 ];
 
 export const Home = HomePage;
@@ -32,11 +36,12 @@ export * from './onboard/onboard';
 export * from './termo/termo';
 
 export {
-  HomePage,
-  LoginPage,
-  ErroConectividadePage,
-  UserListPage,
-  CardPage,
-  ProfilePage,
-  RecoveryPasswordPage
+    HomePage,
+    LoginPage,
+    ErroConectividadePage,
+    UserListPage,
+    CardPage,
+    ProfilePage,
+    RecoveryPasswordPage,
+    UserDetailPage
 };
