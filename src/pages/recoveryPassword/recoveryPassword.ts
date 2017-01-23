535cea4144f4b3cd774834ce729e11cad997a370
fsdrumond <fsdrumond@gmail.com>
2017-01-23 17:35:44 -0200

    feat(recoverypassword): implementada as funcoes de navegacao e notificacao

--- src/pages/recoveryPassword/recoveryPassword.ts
+++ src/pages/recoveryPassword/recoveryPassword.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { ToastController, NavController } from "ionic-angular";
 
 @Component({
   selector: 'page-recovery-password',
   templateUrl: 'recoveryPassword.html'
 })
 export class RecoveryPasswordPage {
-  constructor() {}
+  constructor(private toastCtrl: ToastController, private navCtrl: NavController) {}
+
+  /**
+   * Função para recuperação de senha do usuário.
+   */
+  recoveryPassoword()
+  {
+      let toast = this.toastCtrl.create({
+        message: 'E-mail enviado com sucesso!',
+        duration: 3000,
+        position: 'top'
+      });
+
+      toast.present();
+      this.navCtrl.pop();
+  }
 }
