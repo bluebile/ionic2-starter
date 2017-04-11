2dec894a6fd300795dd3336c93f7e2c8389962f2
ramonornela <ramon.ornela@gmail.com>
2017-04-11 11:54:34 -0300

    chore(*): deletando imagens

--- src/pages/onboard/onboard.ts
+++ src/pages/onboard/onboard.ts
@@ -14,16 +14,13 @@ export class OnboardPage {
 
   slides = [{
     title: 'Bem vindo a aplicação Starter MBA!',
-    description: 'A <b>Ionic-Starter</b> irá apresentar para você alguns exemplos de implementação de recursos ionic.',
-    image: 'assets/img/ica-slidebox-img-1.png'
+    description: 'A <b>Ionic-Starter</b> irá apresentar para você alguns exemplos de implementação de recursos ionic.'
   }, {
     title: 'O que já foi implementado?',
-    description: 'A <b>Ionic-Starter</b> Já possui implementado além de diversos recursos de acesso a dados e controle de erros algumas páginas para simplificar seu trabalho.',
-    image: 'assets/img/ica-slidebox-img-2.png'
+    description: 'A <b>Ionic-Starter</b> Já possui implementado além de diversos recursos de acesso a dados e controle de erros algumas páginas para simplificar seu trabalho.'
   }, {
     title: 'Como posso contribuir?',
-    description: 'O projeto <b>Ionic-Starter</b> é o projeto base criado pelos desenvolvedores da MBA, e toda contribuição e bem vinda, siga as recomendações e ajude a deixar a solução cada vez mais completa.',
-    image: 'assets/img/ica-slidebox-img-3.png'
+    description: 'O projeto <b>Ionic-Starter</b> é o projeto base criado pelos desenvolvedores da MBA, e toda contribuição e bem vinda, siga as recomendações e ajude a deixar a solução cada vez mais completa.'
   }];
 
   constructor(private app: App, private storage: Storage, private auth: Authentication) {}
