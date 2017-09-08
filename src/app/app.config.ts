265adcfa0bf6b70b9327fedb58c2c289c2558450
viniciusgandrade <viniciusgandrade@gmail.com>
2017-09-08 16:49:30 -0300

    fix(*): removendo chaves do app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 // TODO link para documentação do URL resolver com docs e exemplos
 export const APP_ROUTES_DEV = {
   '_defaults': {
-    'host': 'http://ec2-52-67-203-191.sa-east-1.compute.amazonaws.com:8080/rest/',
+    'host': '',
     'variables': {
-      'mm-url': 'http://ec2-52-67-203-191.sa-east-1.compute.amazonaws.com:8080'
+      'mm-url': ''
     }
   },
   'notification-register-client': {
@@ -38,32 +38,32 @@ export const APP_ROUTES_DEV = {
     'method': 'GET'
   },
   'mmmessage': {
-    'url': 'http://ec2-52-67-203-191.sa-east-1.compute.amazonaws.com:8080/mba-mmmessage'
+    'url': '{mm-url}/mba-mmmessage'
   }
 };
 
 export const APP_ROUTES_PROD = {
   '_defaults': {
-    'host': 'http://ec2-52-67-203-191.sa-east-1.compute.amazonaws.com:8080/rest/'
+    'host': ''
   }
 };
 
 export const AppConfig = {
   preloadModules: true,
   onesingalAppId: {
-    dev: '19677f2e-9e7d-4187-9a3a-ba226200ae07',
-    staging: '19677f2e-9e7d-4187-9a3a-ba226200ae07',
-    production: '19677f2e-9e7d-4187-9a3a-ba226200ae07',
+    dev: '',
+    staging: '',
+    production: '',
   },
   mmApiKey: {
-    dev: 'aeb1f4c55bb757427d66ae1094d732d781f78af5',
-    staging: 'aeb1f4c55bb757427d66ae1094d732d781f78af5',
-    production: 'aeb1f4c55bb757427d66ae1094d732d781f78af5'
+    dev: '',
+    staging: '',
+    production: ''
   },
   googleProjectNumber: {
-    dev: '101349642110',
-    staging: '101349642110',
-    production: '101349642110',
+    dev: '',
+    staging: '',
+    production: '',
   },
   urlResolver: {
     dev: APP_ROUTES_DEV,
