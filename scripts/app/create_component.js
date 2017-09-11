0634188b1d73d338a6f2611205da0b2ff5e379c0
viniciusgandrade <viniciusgandrade@gmail.com>
2017-09-11 18:38:24 -0300

    Ajuste path script

--- scripts/app/create_component.js
+++ scripts/app/create_component.js
@@ -7,11 +7,11 @@ var download = require('download');
 console.log('Iniciando download do componente...');
 download('https://github.com/mbamobi/mba-componentes/archive/master.zip', '.tmp', { extract: true }).then(() => {
   console.log('Download concluído. Copiando para pasta do projeto...');
-  console.log(path.join('.tmp/mba-componentes-master', args[2], 'src'));
-  shelljs.cp('-R', path.join('.tmp/mba-componentes-master', args[2], 'src') + '/*', 'src');
+  console.log(path.join('.tmp/mba-componentes-master/components', args[2], 'src'));
+  shelljs.cp('-R', path.join('.tmp/mba-componentes-master/components', args[2], 'src') + '/*', 'src');
   try {
     console.log('Instalando dependências...');
-    var packageJSON = require(path.join('../../.tmp/mba-componentes-master', args[2]) + '/package.json');
+    var packageJSON = require(path.join('../../.tmp/mba-componentes-master/components', args[2]) + '/package.json');
     var dependencies = packageJSON.dependencies;
     var devDependencies = packageJSON.devDependencies;
     if (dependencies) {
@@ -21,12 +21,11 @@ download('https://github.com/mbamobi/mba-componentes/archive/master.zip', '.tmp'
     }
     if (devDependencies) {
       for (var idx in devDependencies) {
-        shelljs.exec('npm install --save ' + idx + '@' + devDependencies[idx]);
+        shelljs.exec('npm install --save-dev ' + idx + '@' + devDependencies[idx]);
       }
     }
   } catch(e) {
     console.log ('Não encontrado package.json');
-    console.log(e);
   }
   shelljs.rm('-rf', '.tmp');
   console.log('Done!');
