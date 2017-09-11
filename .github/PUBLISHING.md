4f6c81da523cfe858cd82053f627787fba720e45
viniciusgandrade <viniciusgandrade@gmail.com>
2017-09-11 19:06:51 -0300

    template npm module

---
+++ .github/PUBLISHING.md
@@ -0,0 +1,10 @@
+# Bulding anp publishing a module
+
+* Change the description in your package.json
+* Change the README file
+* Run the following command:
+```sh
+npm run publishPackage
+```
+
+* Done!
--- scripts/app/copy.config.js
+++
@@ -1,9 +0,0 @@
-
-// https://www.npmjs.com/package/fs-extra
-
-module.exports = {
-  copyIntl: {
-    src: ['node_modules/intl/dist/Intl.min.js', 'node_modules/intl/locale-data/jsonp/pt-BR.js'],
-    dest: '{{BUILD}}/intl'
-  }
-}
